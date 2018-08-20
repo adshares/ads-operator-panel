18007cee409d9f5c4b7020cef9d93cc9246088a2
Przemyslaw Furtak <przemyslawf@gmail.com>
2018-08-20 09:48:11 +0200

    Block view page

--- app/containers/BlockPage/messages.js
+++ app/containers/BlockPage/messages.js
@@ -9,6 +9,66 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   header: {
     id: 'app.containers.BlockPage.header',
-    defaultMessage: 'This is BlockPage container !',
+    defaultMessage: 'Block',
+  },
+  metaTitle: {
+    id: 'app.containers.BlockPage.metaTitle',
+    defaultMessage: 'Block {id}',
+  },
+  metaDescription: {
+    id: 'app.containers.BlockPage.metaDescription',
+    defaultMessage: 'Block {id} page',
+  },
+  messageTabTitle: {
+    id: 'app.containers.BlockPage.messageTabTitle',
+    defaultMessage: 'Messages',
+  },
+  fieldId: {
+    id: 'app.containers.BlockPage.fieldId',
+    defaultMessage: 'Id',
+  },
+  fieldMessageCount: {
+    id: 'app.containers.BlockPage.fieldMessageCount',
+    defaultMessage: 'Messages',
+  },
+  fieldNodeCount: {
+    id: 'app.containers.BlockPage.fieldNodeCount',
+    defaultMessage: 'Nodes',
+  },
+  fieldTransactionCount: {
+    id: 'app.containers.BlockPage.fieldTransactionCount',
+    defaultMessage: 'Transactions',
+  },
+  fieldDividendBalance: {
+    id: 'app.containers.BlockPage.fieldDividendBalance',
+    defaultMessage: 'Dividend Balance',
+  },
+  fieldDividendPay: {
+    id: 'app.containers.BlockPage.fieldDividendPay',
+    defaultMessage: 'Dividend Pay',
+  },
+  fieldTime: {
+    id: 'app.containers.BlockPage.fieldTime',
+    defaultMessage: 'Time',
+  },
+  columnMessagesId: {
+    id: 'app.containers.BlockPage.columnMessagesId',
+    defaultMessage: 'Id',
+  },
+  columnMessagesNodeId: {
+    id: 'app.containers.BlockPage.columnMessagesNodeId',
+    defaultMessage: 'Node Id',
+  },
+  columnMessagesBlockId: {
+    id: 'app.containers.BlockPage.columnMessagesBlockId',
+    defaultMessage: 'Block Id',
+  },
+  columnMessagesTransactionCount: {
+    id: 'app.containers.BlockPage.columnMessagesTransactionCount',
+    defaultMessage: 'Transactions',
+  },
+  columnMessagesLength: {
+    id: 'app.containers.BlockPage.columnMessagesLength',
+    defaultMessage: 'Length',
   },
 });
--- app/containers/BlockPage/tests/index.test.js
+++ app/containers/BlockPage/tests/index.test.js
@@ -1,10 +1,68 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-
-// import { BlockPage } from '../index';
+import React from 'react';
+import sinon from 'sinon';
+import { shallowIntlWrap } from 'testHelper';
+import { BlockPage } from '../index';
 
 describe('<BlockPage />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
+  it('should render h3, DetailView, LatestPanel elements', () => {
+    const match = {
+      params: {},
+    };
+
+    const dispatch = () => {};
+    const messages = {
+      loading: false,
+      error: false,
+      data: [],
+    };
+    const block = {
+      loading: false,
+      error: false,
+      data: {},
+    };
+
+    const renderedComponent = shallowIntlWrap(
+      <BlockPage
+        match={match}
+        dispatch={dispatch}
+        block={block}
+        messages={messages}
+      />,
+    );
+    expect(renderedComponent.find('h3').length).toEqual(1);
+    expect(renderedComponent.find('DetailView').length).toEqual(1);
+    expect(renderedComponent.find('LatestPanel').length).toEqual(1);
+  });
+
+  it('should dispatch loadBlock and loadMessages when id exists', () => {
+    const match = {
+      params: {
+        id: 'ABC11234',
+      },
+    };
+
+    const messages = {
+      loading: false,
+      error: false,
+      data: [],
+    };
+    const block = {
+      loading: false,
+      error: false,
+      data: {},
+    };
+
+    const dispatch = sinon.spy();
+
+    shallowIntlWrap(
+      <BlockPage
+        match={match}
+        dispatch={dispatch}
+        block={block}
+        messages={messages}
+      />,
+    );
+
+    expect(dispatch.callCount).toEqual(2);
   });
 });
