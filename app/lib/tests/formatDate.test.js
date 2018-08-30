11ecf7452d923db8f0c56f733c535c36d9b1087e
Przemyslaw Furtak <przemyslawf@gmail.com>
2018-08-30 14:37:19 +0200

    Change test's description

--- app/lib/tests/formatDate.test.js
+++ app/lib/tests/formatDate.test.js
@@ -13,7 +13,7 @@ describe('formatDate', () => {
     expect(formatDate(date)).toEqual('2018-07-07 15:15:00');
   });
 
-  it('When there is no seconds it should set 00', () => {
+  it('should return null when date is not in ISO8601 format', () => {
     const date = '2018 07 07';
 
     expect(formatDate(date)).toEqual(null);
