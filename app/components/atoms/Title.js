c8034548894f109f1b0240c47c32db23f96b80ac
Meskat <marciniszyn.k@gmail.com>
2018-09-21 17:15:59 +0200

    Table rows style improvements, lists responsive styles

---
+++ app/components/atoms/Title.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  margin: 0 0 0 calc(var(--spacing-factor) * 4);
+  line-height: 1;
+  color: ${props => props.color || `var(--dark-blue)`};
+`;
+
+Title.displayName = 'Title';
