6b2397ed9008e67645f2a91f558a729b8bb0d49c
Meskat <marciniszyn.k@gmail.com>
2018-09-27 15:06:58 +0200

    Animate tables only on first render

--- app/components/LoadingIndicator/tests/Wrapper.test.js
+++ app/components/LoadingIndicator/tests/Wrapper.test.js
@@ -2,27 +2,27 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Wrapper from '../Wrapper';
+import { LoaderWrapper } from '../styled';
 
 describe('<Wrapper />', () => {
   it('should render an <div> tag', () => {
-    const renderedComponent = shallow(<Wrapper />);
+    const renderedComponent = shallow(<LoaderWrapper />);
     expect(renderedComponent.type()).toEqual('div');
   });
 
   it('should have a className attribute', () => {
-    const renderedComponent = shallow(<Wrapper />);
+    const renderedComponent = shallow(<LoaderWrapper />);
     expect(renderedComponent.prop('className').length).toBeGreaterThan(0);
   });
 
   it('should adopt a valid attribute', () => {
     const id = 'test';
-    const renderedComponent = shallow(<Wrapper id={id} />);
+    const renderedComponent = shallow(<LoaderWrapper id={id} />);
     expect(renderedComponent.prop('id')).toEqual(id);
   });
 
   it('should not adopt an invalid attribute', () => {
-    const renderedComponent = shallow(<Wrapper attribute={'test'} />);
+    const renderedComponent = shallow(<LoaderWrapper attribute={'test'} />);
     expect(renderedComponent.prop('attribute')).toBeUndefined();
   });
 });
