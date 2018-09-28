c277e7df3845dcea0365a3c9386fcb068809b8d6
Meskat <marciniszyn.k@gmail.com>
2018-09-28 13:37:15 +0200

    Remove unnecessary part of condition

--- app/components/molecules/Table/TableCell.js
+++ app/components/molecules/Table/TableCell.js
@@ -23,7 +23,7 @@ class TableCell extends React.PureComponent {
     this.checkTextOverflow();
   }
   componentWillUpdate(nextProps) {
-    if (nextProps.breakpoint && (nextProps.breakpoint.name !== this.props.breakpoint.name)) {
+    if (nextProps.breakpoint.name !== this.props.breakpoint.name) {
       this.checkTextOverflow();
     }
   }
