710dfecf2ec034a13022538ca5cdfe6b20f5039c
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-11-07 18:26:48 +0100

    make it quiet

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -3,16 +3,16 @@
 # Ubuntu 18.04 only
 
 # Install dependencies for yarn operations
-apt-get install -y nodejs npm libpng-dev
+apt-get -qq -y install nodejs npm libpng-dev
 
 # envsubst binary
-apt-get install -y gettext-base
+apt-get -qq -y install gettext-base
 
 # Get yarn
-apt-get install -y curl
+apt-get -qq -y install curl
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
 # Install yarn
-apt-get update && apt-get install -y yarn
+apt-get update && apt-get -qq -y install yarn
