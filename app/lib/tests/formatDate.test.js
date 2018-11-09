9df3b70d1484589bc3e35dc2ebd54cd9c2442c81
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2018-11-09 10:59:38 +0100

    UI & time format

--- app/lib/tests/formatDate.test.js
+++ app/lib/tests/formatDate.test.js
@@ -4,13 +4,13 @@ describe('formatDate', () => {
   it('should have correct format date', () => {
     const date = '2018-07-07T15:15:15+02:00';
 
-    expect(formatDate(date)).toEqual('2018-07-07 15:15:15');
+    expect(formatDate(date)).toEqual('2018-07-07 13:15:15');
   });
 
   it('When there is no seconds it should set 00', () => {
     const date = '2018-07-07T15:15+02:00';
 
-    expect(formatDate(date)).toEqual('2018-07-07 15:15:00');
+    expect(formatDate(date)).toEqual('2018-07-07 13:15:00');
   });
 
   it('should return null when date is not in ISO8601 format', () => {
